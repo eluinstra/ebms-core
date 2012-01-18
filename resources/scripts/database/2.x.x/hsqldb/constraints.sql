d8ad9d793f83ee042dd4e260f6b43c4e3708f786
eluinstra <e.luinstra@clockwork.nl>
2012-01-18 16:28:05 +0100

    updated sql scripts

--- resources/scripts/database/2.x.x/hsqldb/constraints.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ebms_message ADD CONSTRAINT unique_ebms_message_conversation_id UNIQUE (conversation_id, cpa_id, sequence_nr);
-
-ALTER TABLE ebms_channel ADD CONSTRAINT unique_ebms_channel_cpa_id_action_id UNIQUE (cpa_id, action_id);
