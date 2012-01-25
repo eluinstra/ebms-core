4141c738a1aedf506f8ec3f909134d79f2673964
eluinstra <e.luinstra@clockwork.nl>
2012-01-25 20:53:39 +0100

    improved cpa file import

---
+++ src/main/java/nl/clockwork/mule/ebms/filter/CPAExistsFilter.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright 2011 Clockwork
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package nl.clockwork.mule.ebms.filter;
+
+import nl.clockwork.common.dao.DAOException;
+import nl.clockwork.mule.ebms.Constants;
+import nl.clockwork.mule.ebms.dao.EbMSDAO;
+import nl.clockwork.mule.ebms.model.cpp.cpa.CollaborationProtocolAgreement;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mule.api.MuleMessage;
+import org.mule.api.routing.filter.Filter;
+
+public class CPAExistsFilter implements Filter
+{
+	protected transient Log logger = LogFactory.getLog(getClass());
+	private EbMSDAO ebMSDAO;
+
+	@Override
+	public boolean accept(MuleMessage message)
+	{
+		try
+		{
+			String cpaId = (String)message.getProperty(Constants.CPA_ID);
+			CollaborationProtocolAgreement cpa = ebMSDAO.getCPA(cpaId);
+			return cpa != null;
+		}
+		catch (DAOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void setEbMSDAO(EbMSDAO ebMSDAO)
+	{
+		this.ebMSDAO = ebMSDAO;
+	}
+}
