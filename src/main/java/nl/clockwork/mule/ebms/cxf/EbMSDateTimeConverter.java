07f5fa00de2a168ffa8c6deb2f090d3a02d3d40d
Richard Kettelerij <r.kettelerij@avisi.nl>
2012-10-08 14:22:12 +0200

    Fixed thread-safety in date/time converter

--- src/main/java/nl/clockwork/mule/ebms/cxf/EbMSDateTimeConverter.java
+++ src/main/java/nl/clockwork/mule/ebms/cxf/EbMSDateTimeConverter.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright 2011 Clockwork
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
  ******************************************************************************/
 package nl.clockwork.mule.ebms.cxf;
 
@@ -25,42 +22,33 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
-public class EbMSDateTimeConverter
-{
-	private static DateFormat df;
-	
-	static
-	{
-		df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		df.setTimeZone(TimeZone.getTimeZone("GMT"));
-		//df.setLenient(true);
-	}
+public class EbMSDateTimeConverter {
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-	public static XMLGregorianCalendar parseDateTime(String date)
-	{
-		try
-		{
-			date = date.replaceFirst("\\.\\d{0,3}","");
-			if (!date.endsWith("Z"))
-				date += "Z";
-			GregorianCalendar calendar = new GregorianCalendar();
-			calendar.setTime(df.parse(date));
-			return DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
-		}
-		catch (ParseException e)
-		{
-			//throw new RuntimeException(e);
-			return null;
-		}
-		catch (DatatypeConfigurationException e)
-		{
-			//throw new RuntimeException(e);
-			return null;
-		}
-	}
+    public static XMLGregorianCalendar parseDateTime(String date) {
+        try {
+            date = date.replaceFirst("\\.\\d{0,3}", "");
+            if (!date.endsWith("Z"))
+                date += "Z";
+            GregorianCalendar calendar = new GregorianCalendar();
+            DateFormat parser = new SimpleDateFormat(DATE_FORMAT);
+            parser.setTimeZone(TimeZone.getTimeZone("GMT"));
+            // df.setLenient(true);
+            calendar.setTime(parser.parse(date));
+            return DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+        } catch (ParseException e) {
+            // throw new RuntimeException(e);
+            return null;
+        } catch (DatatypeConfigurationException e) {
+            // throw new RuntimeException(e);
+            return null;
+        }
+    }
 
-	public static String printDateTime(XMLGregorianCalendar date)
-	{
-		return df.format(date.toGregorianCalendar().getTime());
-	}
+    public static String printDateTime(XMLGregorianCalendar date) {
+        DateFormat printer = new SimpleDateFormat(DATE_FORMAT);
+        printer.setTimeZone(TimeZone.getTimeZone("GMT"));
+        // df.setLenient(true);
+        return printer.format(date.toGregorianCalendar().getTime());
+    }
 }
