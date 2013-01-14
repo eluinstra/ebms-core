49a9e07d6aa56ee6d897c295ca71efff6931f56a
eluinstra <edwin.luinstra@clockwork.nl>
2013-01-14 19:38:14 +0100

    added new EbMSMessageStatusFAILED

--- resources/scripts/database/2.0.5/mssql.sql
+++ resources/scripts/database/2.0.5/mssql.sql
@@ -1,3 +1,8 @@
 DROP INDEX ebms_message.i_message_id;
 
 ALTER TABLE ebms_message ADD CONSTRAINT uc_ebms_message_id UNIQUE (message_id);
+
+UPDATE ebms_message
+SET status=5
+WHERE status=1
+OR status=2;
--- resources/scripts/database/2.0.5/mysql.sql
+++ resources/scripts/database/2.0.5/mysql.sql
@@ -1,3 +1,8 @@
 DROP INDEX i_message_id ON ebms_message;
 
 ALTER TABLE ebms_message ADD CONSTRAINT uc_ebms_message_id UNIQUE (message_id);
+
+UPDATE ebms_message
+SET status=5
+WHERE status=1
+OR status=2;
