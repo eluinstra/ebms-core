80518eb70c12070f494086e785f0ed368879c794
eluinstra <edwin.luinstra@clockwork.nl>
2013-01-14 13:33:35 +0100

    moved class to stub package

--- src/main/java/nl/clockwork/mule/ebms/stub/ebf/filter/EbMSMessageContentXSDValidationFilter.java
+++ src/main/java/nl/clockwork/mule/ebms/stub/ebf/filter/EbMSMessageContentXSDValidationFilter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package nl.clockwork.mule.ebms.filter;
+package nl.clockwork.mule.ebms.stub.ebf.filter;
 
 import java.util.List;
 
