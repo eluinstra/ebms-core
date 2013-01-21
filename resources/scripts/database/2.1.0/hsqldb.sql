edbfa3933e023598569fe7744b764eb70185617b
eluinstra <edwin.luinstra@clockwork.nl>
2013-01-21 01:10:46 +0100

    improved update script

--- resources/scripts/database/2.1.0/hsqldb.sql
+++ resources/scripts/database/2.1.0/hsqldb.sql
@@ -1,1 +1,5 @@
+ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NULL;
+
+UPDATE ebms_attachment SET content_id = 'cid:1';
+
 ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NOT NULL;
