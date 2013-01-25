f462900321be3987768fe381255c8f22406d17c2
eluinstra <edwin.luinstra@clockwork.nl>
2013-01-25 21:56:10 +0100

    updated db scripts (for version 2.0.5)

--- resources/scripts/database/2.0.5/mysql.sql
+++ resources/scripts/database/2.0.5/mysql.sql
@@ -1,3 +1,24 @@
+DELETE FROM ebms_send_event
+WHERE ebms_message_id NOT IN (
+	SELECT min(id)
+	FROM ebms_message
+	GROUP BY message_id
+);
+
+DELETE FROM ebms_attachment
+WHERE ebms_message_id NOT IN (
+	SELECT min(id)
+	FROM ebms_message
+	GROUP BY message_id
+);
+
+DELETE FROM ebms_message
+WHERE id NOT IN (
+	SELECT min(id)
+	FROM ebms_message
+	GROUP BY message_id
+);
+
 DROP INDEX i_message_id ON ebms_message;
 
 ALTER TABLE ebms_message ADD CONSTRAINT uc_ebms_message_id UNIQUE (message_id);
