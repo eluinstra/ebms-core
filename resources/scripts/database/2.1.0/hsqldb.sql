a6095988d9932d8a468fcfbe5e22fc5510db425f
eluinstra <edwin.luinstra@clockwork.nl>
2013-01-31 17:05:15 +0100

    cleanup

--- resources/scripts/database/2.1.0/hsqldb.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NULL;
-
-UPDATE ebms_attachment SET content_id = 'cid:1';
-
-ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NOT NULL;
