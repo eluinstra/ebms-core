b6c87bc26f1c260b4938e4034dc3f1eb79a74b19
eluinstra <edwin.luinstra@clockwork.nl>
2013-02-06 14:39:36 +0100

    implemented EbMSMessageProcessing and CPAInserter improved cpa import flow

---
+++ src/main/java/nl/clockwork/mule/ebms/job/EbMSMessageProcessing.java
@@ -0,0 +1,58 @@
+package nl.clockwork.mule.ebms.job;
+
+import java.util.List;
+
+import nl.clockwork.ebms.iface.EbMSMessageService;
+import nl.clockwork.ebms.job.Job;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleException;
+import org.mule.api.context.MuleContextAware;
+import org.mule.module.client.MuleClient;
+
+public class EbMSMessageProcessing implements Job, MuleContextAware
+{
+  protected transient Log logger = LogFactory.getLog(getClass());
+	private MuleContext muleContext;
+	private EbMSMessageService ebMSMessageService;
+	private String delegatePath;
+
+	@Override
+	public void execute()
+	{
+		logger.info("Executing job: " + this.getClass());
+		try
+		{
+			MuleClient client = new MuleClient(muleContext);
+			List<String> messageIds = ebMSMessageService.getMessageIds(null,null);
+			for (String messageId : messageIds)
+			{
+				client.send(delegatePath,ebMSMessageService.getMessage(messageId,false),null);
+				ebMSMessageService.processMessage(messageId);
+			}
+		}
+		catch (MuleException e)
+		{
+			logger.error("",e);
+		}
+	}
+
+	@Override
+	public void setMuleContext(MuleContext muleContext)
+	{
+		this.muleContext = muleContext;
+	}
+	
+	public void setEbMSMessageService(EbMSMessageService ebMSMessageService)
+	{
+		this.ebMSMessageService = ebMSMessageService;
+	}
+	
+	public void setDelegatePath(String delegatePath)
+	{
+		this.delegatePath = delegatePath;
+	}
+
+}
---
+++ src/main/java/nl/clockwork/mule/ebms/stub/ebf/processor/CPAInserter.java
@@ -0,0 +1,79 @@
+package nl.clockwork.mule.ebms.stub.ebf.processor;
+
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import nl.clockwork.ebms.iface.CPAService;
+import nl.clockwork.ebms.iface.CPAServiceException;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+import org.xml.sax.SAXException;
+
+public class CPAInserter implements Callable
+{
+	private Schema schema;
+	private CPAService cpaService;
+	private Object validatorMonitor = new Object();
+
+	public CPAInserter() throws SAXException
+	{
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		//schema = factory.newSchema(new StreamSource(this.getClass().getResourceAsStream(xsdFile)));
+    String systemId = this.getClass().getResource("/nl/clockwork/ebms/xsd/cpp-cpa-2_0.xsd").toString();
+		schema = factory.newSchema(new StreamSource(this.getClass().getResourceAsStream("/nl/clockwork/ebms/xsd/cpp-cpa-2_0.xsd"),systemId));
+	}
+	
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception
+	{
+		MuleMessage message = eventContext.getMessage();
+		try
+		{
+			//String cpa = (String)message.getPayload();
+			//String cpa = IOUtils.toString((InputStream)message.getPayload());
+			String cpa = message.getPayloadAsString();
+			//quick fix for synchronization problem with validate() method
+			synchronized (validatorMonitor)
+			{
+				Validator validator = schema.newValidator();
+				try
+				{
+					validator.validate(new StreamSource(new StringReader(cpa)));
+					cpaService.insertCPA(cpa,true);
+					message.setProperty("EBMS.REPORT",message.getProperty("originalFilename") + " inserted successfully.");
+				}
+				catch (SAXException e)
+				{
+					message.setProperty("EBMS.REPORT",message.getProperty("originalFilename") + " contains not a valid CPA.");
+				}
+				return message;
+			}
+		}
+		catch (CPAServiceException e)
+		{
+			Writer result = new StringWriter();
+			if (message.getExceptionPayload() != null)
+			{
+				PrintWriter pw = new PrintWriter(result);
+				e.printStackTrace(pw);
+			}
+			message.setProperty("EBMS.REPORT","Update " + message.getProperty("originalFilename") + " failed.\n\n" + result.toString());
+			throw e;
+		}
+	}
+
+	public void setCpaService(CPAService cpaService)
+	{
+		this.cpaService = cpaService;
+	}
+}
