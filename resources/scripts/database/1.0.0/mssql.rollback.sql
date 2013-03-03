18ec76742b1dd9b47d71ffa69e299031f68572f9
eluinstra <edwin.luinstra@clockwork.nl>
2013-03-03 23:52:50 +0100

    fixed scripts

--- resources/scripts/database/1.0.0/mssql.rollback.sql
+++ resources/scripts/database/1.0.0/mssql.rollback.sql
@@ -2,4 +2,4 @@ ALTER TABLE ebms_message DROP COLUMN time_to_live;
 
 ALTER TABLE ebms_message ADD original BLOB NULL;
 
-ALTER TABLE ebms_attachment MODIFY name VARCHAR(256) NOT NULL;
+ALTER TABLE ebms_attachment ALTER COLUMN name VARCHAR(256) NOT NULL;
--- resources/scripts/database/1.0.0/mssql.sql
+++ resources/scripts/database/1.0.0/mssql.sql
@@ -2,4 +2,4 @@ ALTER TABLE ebms_message ADD time_to_live DATETIME NULL;
 
 ALTER TABLE ebms_message DROP COLUMN original;
 
-ALTER TABLE ebms_attachment MODIFY name VARCHAR(256) NULL;
+ALTER TABLE ebms_attachment ALTER COLUMN name VARCHAR(256) NULL;
