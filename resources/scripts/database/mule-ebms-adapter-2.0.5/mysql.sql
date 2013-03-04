93023cd61a0d5f12cb5c9eb0980d3a2b8975d249
eluinstra <edwin.luinstra@clockwork.nl>
2013-03-04 22:54:10 +0100

    fixed db scripts mule-ebms-adapter-2.0.5

--- resources/scripts/database/mule-ebms-adapter-2.0.5/mysql.sql
+++ resources/scripts/database/mule-ebms-adapter-2.0.5/mysql.sql
@@ -1,25 +1,29 @@
+CREATE TEMPORARY TABLE ebms_tmp AS
+	SELECT min(id) AS id
+	FROM ebms_message
+	GROUP BY message_id;
+
 DELETE FROM ebms_send_event
 WHERE ebms_message_id NOT IN (
-	SELECT min(id)
-	FROM ebms_message
-	GROUP BY message_id
+	SELECT id
+	FROM ebms_tmp
 );
 
 DELETE FROM ebms_attachment
 WHERE ebms_message_id NOT IN (
-	SELECT min(id)
-	FROM ebms_message
-	GROUP BY message_id
+	SELECT id
+	FROM ebms_tmp
 );
 
 DELETE FROM ebms_message
 WHERE id NOT IN (
-	SELECT min(id)
-	FROM ebms_message
-	GROUP BY message_id
+	SELECT id
+	FROM ebms_tmp
 );
 
-DROP INDEX i_message_id ON ebms_message;
+DROP TABLE ebms_tmp;
+
+-- DROP INDEX i_message_id ON ebms_message;
 
 ALTER TABLE ebms_message ADD CONSTRAINT uc_ebms_message_id UNIQUE (message_id(255));
 
--- resources/scripts/database/mule-ebms-adapter-2.0.5/postgresql.rollback.sql
+++ resources/scripts/database/mule-ebms-adapter-2.0.5/postgresql.rollback.sql
@@ -2,6 +2,6 @@ UPDATE ebms_message
 SET status=2
 WHERE status=5;
 
-ALTER TABLE ebms_message DROP CONSTRAINT uc_ebms_message_id;
+ALTER TABLE ebms_message DROP CONSTRAINT IF EXISTS uc_ebms_message_id;
 
 CREATE INDEX i_message_id ON ebms_message(message_id);
