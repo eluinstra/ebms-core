0cfa2c2f97fcf9b3a469c1ee9ef38ee7ce7c5483
eluinstra <edwin.luinstra@clockwork.nl>
2013-03-04 22:53:31 +0100

    fixed db scripts mule-ebms-adapter-2.1.0

--- resources/scripts/database/mule-ebms-adapter-2.1.0/postgresql.sql
+++ resources/scripts/database/mule-ebms-adapter-2.1.0/postgresql.sql
@@ -2,4 +2,4 @@ ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NULL;
 
 UPDATE ebms_attachment SET content_id = 'cid:1';
 
-ALTER TABLE ebms_attachment ADD content_id VARCHAR(256) NOT NULL;
+ALTER TABLE ebms_attachment ALTER content_id SET NOT NULL;
