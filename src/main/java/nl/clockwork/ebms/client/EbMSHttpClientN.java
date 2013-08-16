998bf5194d7ba95a3a9500c5fdf95d08ecd75e34
eluinstra <edwin.luinstra@clockwork.nl>
2013-08-16 15:23:04 +0200

    implemented EbMSHttpClientN

---
+++ src/main/java/nl/clockwork/ebms/client/EbMSHttpClientN.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright 2011 Clockwork
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package nl.clockwork.ebms.client;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import nl.clockwork.ebms.common.util.DOMUtils;
+import nl.clockwork.ebms.model.EbMSDocument;
+import nl.clockwork.ebms.processor.EbMSProcessorException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class EbMSHttpClientN implements EbMSClient
+{
+  protected transient Log logger = LogFactory.getLog(getClass());
+	private SSLFactoryManager sslFactoryManager;
+	protected boolean chunkedStreamingMode;
+	
+	public EbMSHttpClientN()
+	{
+		//System.setProperty("http.keepAlive","false");
+	}
+
+	public EbMSDocument sendMessage(String uri, EbMSDocument document) throws EbMSProcessorException
+	{
+		HttpURLConnection connection = null;
+		try
+		{
+			connection = (HttpURLConnection)openConnection(uri);
+			//connection.setConnectTimeout(connectTimeout);
+			if (logger.isInfoEnabled())
+				logger.info("OUT:\n" + DOMUtils.toString(document.getMessage()));
+			if (chunkedStreaming(uri))
+				connection.setChunkedStreamingMode(0);
+			EbMSMessageWriter writer = new EbMSMessageWriterImpl(connection);
+			writer.write(document);
+			connection.connect();
+			EbMSMessageReader reader = new EbMSMessageReaderImpl(connection);
+			try
+			{
+				EbMSDocument in = reader.read();
+				if (logger.isInfoEnabled())
+				{
+					logger.info("StatusCode: " + connection.getResponseCode());
+					logger.info("IN:\n" + (in == null || in.getMessage() == null ? "" : DOMUtils.toString(in.getMessage())));
+				}
+				return in;
+			}
+			finally
+			{
+				reader.close();
+			}
+		}
+		catch (ConnectException e)
+		{
+			throw new EbMSProcessorException("Error connecting to: " + uri,e);
+		}
+		catch (Exception e)
+		{
+			throw new EbMSProcessorException(e);
+		}
+		finally
+		{
+			if (connection != null)
+				connection.disconnect();
+		}
+	}
+	
+	public boolean chunkedStreaming(String uri)
+	{
+		return chunkedStreamingMode;
+	}
+
+	@SuppressWarnings({"restriction","deprecation"})
+	private URLConnection openConnection(String uri) throws IOException
+	{
+		URL url = new URL(uri);
+		URLConnection connection = url.openConnection();
+		connection.setDoOutput(true);
+		//connection.setMethod("POST");
+		if (connection instanceof HttpsURLConnection)
+			((HttpsURLConnection)connection).setSSLSocketFactory(sslFactoryManager.getSslFactory());
+		else if (connection instanceof com.sun.net.ssl.HttpsURLConnection)
+			((com.sun.net.ssl.HttpsURLConnection)connection).setSSLSocketFactory(sslFactoryManager.getSslFactory());
+		return connection;
+	}
+
+	public void setSslFactoryManager(SSLFactoryManager sslFactoryManager)
+	{
+		this.sslFactoryManager = sslFactoryManager;
+	}
+	
+	public void setChunkedStreamingMode(boolean chunkedStreamingMode)
+	{
+		this.chunkedStreamingMode = chunkedStreamingMode;
+	}
+}
---
+++ src/main/java/nl/clockwork/ebms/client/EbMSMessageReaderImpl.java
@@ -0,0 +1,126 @@
+package nl.clockwork.ebms.client;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import nl.clockwork.ebms.common.util.DOMUtils;
+import nl.clockwork.ebms.model.EbMSAttachment;
+import nl.clockwork.ebms.model.EbMSDocument;
+import nl.clockwork.ebms.processor.EbMSProcessingException;
+import nl.clockwork.ebms.processor.EbMSProcessorException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class EbMSMessageReaderImpl implements EbMSMessageReader
+{
+	private HttpURLConnection connection;
+	private InputStream input;
+	
+	public EbMSMessageReaderImpl(HttpURLConnection connection)
+	{
+		this.connection = connection;
+	}
+
+	@Override
+	public EbMSDocument read() throws IOException, EbMSProcessorException
+	{
+		try
+		{
+			input = connection.getInputStream();
+			String statusCode = new Integer(connection.getResponseCode()).toString();
+			if (statusCode.startsWith("2"))
+			{
+				EbMSDocument result = getEbMSMessage(input);
+				return result;
+			}
+			else
+			{
+				if (statusCode.startsWith("4") || statusCode.startsWith("5"))
+					throw new EbMSProcessingException("StatusCode: " + connection.getResponseCode() + "\n" + IOUtils.toString(connection.getErrorStream()));
+				else
+					throw new EbMSProcessingException("StatusCode: " + connection.getResponseCode());
+			}
+		}
+		catch (IOException e)
+		{
+			try
+			{
+				input = null;
+				//int responseCode = connection.getResponseCode();
+				InputStream error = new BufferedInputStream(connection.getErrorStream());
+				error.close();
+			}
+			catch (IOException ex)
+			{
+			}
+			throw e;
+		}
+		catch (ParserConfigurationException e)
+		{
+			throw new EbMSProcessorException(e);
+		}
+		catch (SAXException e)
+		{
+			throw new EbMSProcessorException(e);
+		}
+	}
+
+	@Override
+	public void close() throws IOException
+	{
+		if (input != null)
+			input.close();
+	}
+
+	private EbMSDocument getEbMSMessage(InputStream in) throws ParserConfigurationException, SAXException, IOException
+	{
+		EbMSDocument result = null;
+		String message = IOUtils.toString(in,getCharSet());
+		if (StringUtils.isNotBlank(message))
+		{
+			DocumentBuilder db = DOMUtils.getDocumentBuilder();
+			Document d = db.parse(new InputSource(new StringReader(message)));
+			result = new EbMSDocument(d,new ArrayList<EbMSAttachment>());
+		}
+		return result;
+	}
+	
+	private String getCharSet()
+	{
+		String contentType = getHeaderField(connection,"Content-Type");
+		String charset = null;
+		for (String param: contentType.replace(" ","").split(";"))
+		{
+			if (param.startsWith("charset="))
+			{
+				charset = param.split("=",2)[1];
+				break;
+			}
+		}
+		return charset;
+	}
+	
+	private String getHeaderField(HttpURLConnection connection, String name)
+	{
+		String result = connection.getHeaderField(name);
+		if (result == null)
+			for (String key : connection.getHeaderFields().keySet())
+				if (key.equalsIgnoreCase(name))
+				{
+					result = connection.getHeaderField(key);
+					break;
+				}
+		return result;
+	}
+}
