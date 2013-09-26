ed00a2a6d0a73d70b39bdf9111dd283f568b3a54
eluinstra <edwin.luinstra@clockwork.nl>
2013-09-26 22:23:50 +0200

    renamed ProcessEvents

--- src/main/java/nl/clockwork/ebms/job/ProcessEbMSEvents.java
+++ src/main/java/nl/clockwork/ebms/job/ProcessEbMSEvents.java
@@ -44,7 +44,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oasis_open.committees.ebxml_cppa.schema.cpp_cpa_2_0.CollaborationProtocolAgreement;
 
-public class ProcessEvents implements Job
+public class ProcessEbMSEvents implements Job
 {
 	private class SendEventJob implements Runnable
 	{
