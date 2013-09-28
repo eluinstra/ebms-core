3f37cf2b93fc43cbd7402648b201f2be73718ad1
eluinstra <edwin.luinstra@clockwork.nl>
2013-09-29 01:16:14 +0200

    fixed bug in mysql script

--- src/main/java/nl/clockwork/ebms/dao/mysql/EbMSDAOImpl.java
+++ src/main/java/nl/clockwork/ebms/dao/mysql/EbMSDAOImpl.java
@@ -15,6 +15,11 @@
  ******************************************************************************/
 package nl.clockwork.ebms.dao.mysql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import nl.clockwork.ebms.Constants.EbMSMessageStatus;
 import nl.clockwork.ebms.dao.AbstractEbMSDAO;
 import nl.clockwork.ebms.dao.ConnectionManager;
@@ -49,4 +54,34 @@ public class EbMSDAOImpl extends AbstractEbMSDAO
 		" order by time_stamp asc" +
 		" limit " + maxNr;
 	}
+
+	protected PreparedStatement getInsertDuplicateMessagePreparedStatement(Connection connection) throws SQLException
+	{
+		return connection.prepareStatement
+		(
+			"insert into ebms_message (" +
+				"time_stamp," +
+				"cpa_id," +
+				"conversation_id," +
+				"sequence_nr," +
+				"message_id," +
+				"message_nr," +
+				"ref_to_message_id," +
+				"time_to_live," +
+				"from_role," +
+				"to_role," +
+				"service_type," +
+				"service," +
+				"action," +
+				"signature," +
+				"message_header," +
+				"sync_reply," +
+				"message_order," +
+				"ack_requested," +
+				"content" +
+			") values (?,?,?,?,?,(select nr from (select max(message_nr) + 1 as nr from ebms_message where message_id = ?) as c),?,?,?,?,?,?,?,?,?,?,?,?,?)",
+			Statement.RETURN_GENERATED_KEYS
+		);
+	}
+
 }
