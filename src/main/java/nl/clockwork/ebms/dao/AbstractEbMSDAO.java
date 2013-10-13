a8309848e0e64bb30571b62d8581161fafead76e
eluinstra <edwin.luinstra@clockwork.nl>
2013-10-13 22:02:09 +0200

    code improvement

--- src/main/java/nl/clockwork/ebms/dao/AbstractEbMSDAO.java
+++ src/main/java/nl/clockwork/ebms/dao/AbstractEbMSDAO.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.activation.DataSource;
 import javax.mail.util.ByteArrayDataSource;
 import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
@@ -1176,8 +1175,8 @@ public abstract class AbstractEbMSDAO implements EbMSDAO
 				return null;
 			List<EbMSAttachment> attachments = getAttachments(messageId);
 			List<EbMSDataSource> dataSources = new ArrayList<EbMSDataSource>();
-			for (DataSource dataSource : attachments)
-				dataSources.add(new EbMSDataSource(dataSource.getName(),dataSource.getContentType(),IOUtils.toByteArray(dataSource.getInputStream())));
+			for (EbMSAttachment attachment : attachments)
+				dataSources.add(new EbMSDataSource(attachment.getName(),attachment.getContentType(),IOUtils.toByteArray(attachment.getInputStream())));
 			return new EbMSMessageContent(messageContext,dataSources);
 		}
 		catch (SQLException e)
