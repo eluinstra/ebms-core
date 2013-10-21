18d03ebfb255cc7fb5fcb8ce4f5e7ae155d17ff3
eluinstra <edwin.luinstra@clockwork.nl>
2013-10-21 21:54:52 +0200

    fixed returning wrong http status code

--- src/main/java/nl/clockwork/mule/ebms/processor/EbMSHttpHandler.java
+++ src/main/java/nl/clockwork/mule/ebms/processor/EbMSHttpHandler.java
@@ -18,17 +18,15 @@ package nl.clockwork.mule.ebms.processor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 import nl.clockwork.ebms.processor.EbMSMessageProcessor;
 import nl.clockwork.ebms.processor.EbMSProcessorException;
 import nl.clockwork.ebms.server.EbMSInputStreamHandler;
-import nl.clockwork.ebms.server.EbMSInputStreamHandlerImpl;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpVersion;
 import org.apache.commons.httpclient.ProtocolException;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
@@ -46,64 +44,74 @@ public class EbMSHttpHandler implements Callable
 	public Object onCall(MuleEventContext eventContext) throws Exception
 	{
 		final MuleMessage message = eventContext.getMessage();
-  	final InputStream request = (InputStream)message.getPayload();
-  	final HttpResponse response = new HttpResponse();
+		final InputStream request = (InputStream)message.getPayload();
+		final HttpResponse response = new HttpResponse();
+		final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		try
+		{
+			EbMSInputStreamHandler messageHandler = 
+	  		new EbMSInputStreamHandler(ebMSMessageProcessor)
+				{
+					@Override
+					public String getRequestHeader(String headerName)
+					{
+						String result = message.getProperty(headerName,PropertyScope.INBOUND);
+						if (result == null)
+							for (String key : message.getPropertyNames(PropertyScope.INBOUND))
+							{
+								if (key.equalsIgnoreCase(headerName))
+								{
+									result = message.getProperty(key,PropertyScope.INBOUND);
+									break;
+								}
+							}
+						return result;
+					}
+					
+					@Override
+					public void writeResponseStatus(int statusCode)
+					{
+						try
+						{
+							response.setStatusLine(HttpVersion.parse(HttpConstants.HTTP11),statusCode);
+						}
+						catch (ProtocolException e)
+						{
+						}
+					}
+					
+					@Override
+					public void writeResponseHeader(String name, String value)
+					{
+						response.addHeader(new Header(name,value));
+					}
+					
+					@Override
+					public OutputStream getOutputStream() throws IOException
+					{
+						return output;
+					}
+				}
+	  	;
+			messageHandler.handle(request);
+		}
+		catch (EbMSProcessorException e)
+		{
+			throw new IOException(e);
+		}
+
 		response.setBody(
 			new OutputHandler()
 			{
 				@Override
 				public void write(MuleEvent event, final OutputStream out) throws IOException
 				{
-					try
-					{
-						EbMSInputStreamHandler messageHandler = 
-				  		new EbMSInputStreamHandlerImpl(ebMSMessageProcessor,getHeaders(message))
-							{
-								@Override
-								public void writeResponseStatus(int statusCode)
-								{
-									try
-									{
-										response.setStatusLine(HttpVersion.parse(HttpConstants.HTTP11),statusCode);
-									}
-									catch (ProtocolException e)
-									{
-									}
-								}
-								
-								@Override
-								public void writeResponseHeader(String name, String value)
-								{
-									response.addHeader(new Header(name,value));
-								}
-								
-								@Override
-								public OutputStream getOutputStream() throws IOException
-								{
-									return out;
-								}
-							}
-				  	;
-						messageHandler.handle(request);
-					}
-					catch (EbMSProcessorException e)
-					{
-						throw new IOException(e);
-					}
+					output.writeTo(out);
 				}
 			}
 		);
 		message.setPayload(response);
 		return message;
-	}
-
-	private Map<String,String> getHeaders(MuleMessage message)
-	{
-		Map<String,String> result = new HashMap<String,String>();
-		for (Object name : message.getPropertyNames(PropertyScope.INBOUND))
-			if (message.getProperty((String)name,PropertyScope.INBOUND) instanceof String)
-				result.put((String)name,(String)message.getProperty((String)name,PropertyScope.INBOUND));
-		return result;
 	}
 
 	public void setEbMSMessageProcessor(EbMSMessageProcessor ebMSMessageProcessor)
