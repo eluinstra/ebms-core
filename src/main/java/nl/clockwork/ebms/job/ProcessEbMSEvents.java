0484082cbd6fe174a83fab77abda7bf2088b79a2
eluinstra <edwin.luinstra@clockwork.nl>
2013-12-04 16:12:07 +0100

    improved executor server limited thread queue

--- src/main/java/nl/clockwork/ebms/job/ProcessEbMSEvents.java
+++ src/main/java/nl/clockwork/ebms/job/ProcessEbMSEvents.java
@@ -18,21 +18,23 @@ package nl.clockwork.ebms.job;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-import nl.clockwork.ebms.EventListener;
 import nl.clockwork.ebms.Constants.EbMSEventStatus;
 import nl.clockwork.ebms.Constants.EbMSEventType;
 import nl.clockwork.ebms.Constants.EbMSMessageStatus;
+import nl.clockwork.ebms.EventListener;
 import nl.clockwork.ebms.client.EbMSClient;
 import nl.clockwork.ebms.client.EbMSResponseException;
 import nl.clockwork.ebms.dao.DAOTransactionCallback;
 import nl.clockwork.ebms.dao.EbMSDAO;
 import nl.clockwork.ebms.model.EbMSDocument;
-import nl.clockwork.ebms.model.EbMSMessage;
 import nl.clockwork.ebms.model.EbMSEvent;
+import nl.clockwork.ebms.model.EbMSMessage;
 import nl.clockwork.ebms.processor.EbMSMessageProcessor;
 import nl.clockwork.ebms.processor.EbMSProcessingException;
 import nl.clockwork.ebms.signature.EbMSSignatureGenerator;
@@ -125,7 +127,9 @@ public class ProcessEbMSEvents implements Job
 
   protected transient Log logger = LogFactory.getLog(getClass());
   private ExecutorService executorService;
-	private int maxThreads = 4;
+  private Integer maxThreads;
+	private Integer processorsScaleFactor;
+	private Integer queueScaleFactor;
 	private EventListener eventListener;
   private EbMSDAO ebMSDAO;
 	private EbMSSignatureGenerator signatureGenerator;
@@ -134,7 +138,23 @@ public class ProcessEbMSEvents implements Job
 
 	public void init()
 	{
-		executorService = Executors.newFixedThreadPool(maxThreads);
+		//executorService = Executors.newFixedThreadPool(maxThreads);
+		if (maxThreads == null || maxThreads <= 0)
+		{
+			maxThreads = Runtime.getRuntime().availableProcessors() * processorsScaleFactor;
+			logger.info(this.getClass().getName() + " using " + maxThreads + " threads");
+		}
+		if (processorsScaleFactor == null || processorsScaleFactor <= 0)
+		{
+			processorsScaleFactor = 1;
+			logger.info(this.getClass().getName() + " using processors scale factor " + processorsScaleFactor);
+		}
+		if (queueScaleFactor == null || queueScaleFactor <= 0)
+		{
+			queueScaleFactor = 1;
+			logger.info(this.getClass().getName() + " using queue scale factor " + queueScaleFactor);
+		}
+		executorService = new ThreadPoolExecutor(maxThreads - 1,maxThreads - 1,1,TimeUnit.MINUTES,new ArrayBlockingQueue<Runnable>(maxThreads * queueScaleFactor,true),new ThreadPoolExecutor.CallerRunsPolicy());
 	}
 	
   @Override
@@ -174,9 +194,19 @@ public class ProcessEbMSEvents implements Job
 		);
 	}
 
-	public void setMaxThreads(int maxThreads)
+	public void setMaxThreads(Integer maxThreads)
 	{
 		this.maxThreads = maxThreads;
+	}
+
+	public void setProcessorsScaleFactor(Integer processorsScaleFactor)
+	{
+		this.processorsScaleFactor = processorsScaleFactor;
+	}
+
+	public void setQueueScaleFactor(Integer queueScaleFactor)
+	{
+		this.queueScaleFactor = queueScaleFactor;
 	}
 
 	public void setEventListener(EventListener eventListener)
