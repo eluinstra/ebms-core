571172e11a2a5435ee0fb187aea3be47f9b39f00
eluinstra <edwin.luinstra@clockwork.nl>
2014-08-24 14:37:14 +0200

    removed deprecated code

--- src/main/java/nl/clockwork/ebms/client/EbMSHttpClient.java
+++ src/main/java/nl/clockwork/ebms/client/EbMSHttpClient.java
@@ -88,7 +88,6 @@ public class EbMSHttpClient implements EbMSClient
 		return chunkedStreamingMode;
 	}
 
-	@SuppressWarnings({"restriction","deprecation"})
 	private URLConnection openConnection(String uri) throws IOException
 	{
 		URL url = new URL(uri);
@@ -99,11 +98,6 @@ public class EbMSHttpClient implements EbMSClient
 		{
 			((HttpsURLConnection)connection).setHostnameVerifier(sslFactoryManager.getHostnameVerifier((HttpsURLConnection)connection));
 			((HttpsURLConnection)connection).setSSLSocketFactory(sslFactoryManager.getSslSocketFactory());
-		}
-		else if (connection instanceof com.sun.net.ssl.HttpsURLConnection)
-		{
-			((com.sun.net.ssl.HttpsURLConnection)connection).setHostnameVerifier(sslFactoryManager.getHostnameVerifier((com.sun.net.ssl.HttpsURLConnection)connection));
-			((com.sun.net.ssl.HttpsURLConnection)connection).setSSLSocketFactory(sslFactoryManager.getSslSocketFactory());
 		}
 		return connection;
 	}
