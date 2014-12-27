3c16dd6707d58f547a28143d16c2fd28c52d0dd8
eluinstra <edwin.luinstra@clockwork.nl>
2014-12-27 19:34:48 +0100

    removed ResponseWrapper

--- src/main/java/nl/clockwork/ebms/service/CPAService.java
+++ src/main/java/nl/clockwork/ebms/service/CPAService.java
@@ -21,7 +21,6 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.ResponseWrapper;
 
 @WebService(targetNamespace="http://www.ordina.nl/cpa/2.1")
 public interface CPAService
@@ -36,9 +35,9 @@ public interface CPAService
 	@WebMethod(operationName="DeleteCPA")
 	void deleteCPA(@WebParam(name="CPAId") String cpaId) throws CPAServiceException;
 
-	@WebResult(name="GetCPAIdsResponse")
+	@WebResult(name="CPAIds")
 	@WebMethod(operationName="GetCPAIds")
-	@ResponseWrapper(className="nl.clockwork.ebms.service.GetCPAIdsResponse")
+	//@ResponseWrapper(className="nl.clockwork.ebms.service.GetCPAIdsResponse")
 	List<String> getCPAIds() throws CPAServiceException;
 
 	@WebResult(name="CPA")
