998a42b0f89a713071042a36c3f5e581ea364f67
eluinstra <edwin.luinstra@clockwork.nl>
2015-04-22 20:02:23 +0200

    cleanup

--- src/main/java/nl/clockwork/ebms/dao/spring/AbstractEbMSDAO.java
+++ src/main/java/nl/clockwork/ebms/dao/spring/AbstractEbMSDAO.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.activation.DataSource;
 import javax.mail.util.ByteArrayDataSource;
 import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
