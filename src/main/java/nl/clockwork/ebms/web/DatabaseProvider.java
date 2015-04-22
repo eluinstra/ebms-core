c81cfaba27d743df105c16352f6e65d6bba0d9e4
eluinstra <edwin.luinstra@clockwork.nl>
2015-04-22 11:07:16 +0200

    implementing adapter

---
+++ src/main/java/nl/clockwork/ebms/web/DatabaseProvider.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2011 Clockwork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.clockwork.ebms.web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DatabaseProvider
+{
+	protected transient Log logger = LogFactory.getLog(getClass());
+	private DataSource dataSource;
+	private String driverClassName;
+	private boolean execute;
+	private String[] sqlFiles;
+
+	public void init() throws SQLException, IOException
+	{
+		if (execute && driverClassName.equals(((ComboPooledDataSource)dataSource).getDriverClass()))
+		{
+			try (Connection c = dataSource.getConnection())
+			{
+				for (String sqlFile : sqlFiles)
+				{
+					try (Statement s = c.createStatement())
+					{
+						logger.info("Executing file " + sqlFile);
+						s.executeUpdate(IOUtils.toString(DatabaseProvider.class.getResourceAsStream(sqlFile.trim())));
+					}
+				}
+			}
+			catch (SQLException e)
+			{
+				logger.warn("",e);
+			}
+		}
+	}
+	
+	public void close() throws SQLException
+	{
+		if (execute)
+		{
+			try (Connection c = dataSource.getConnection())
+			{
+				try (Statement s = c.createStatement())
+				{
+					s.executeUpdate("shutdown");
+				}
+			}
+		}
+	}
+	
+	public void setDataSource(DataSource dataSource)
+	{
+		this.dataSource = dataSource;
+	}
+	
+	public void setDriverClassName(String driverClassName)
+	{
+		this.driverClassName = driverClassName;
+	}
+	
+	public void setExecute(boolean execute)
+	{
+		this.execute = execute;
+	}
+	
+	public void setSqlFiles(String sqlFiles)
+	{
+		this.sqlFiles = sqlFiles.split(",");
+	}
+}
---
+++ src/main/resources/nl/clockwork/ebms/web/hsqldb/hsqldb.sql
@@ -0,0 +1,70 @@
+--
+-- Copyright 2011 Clockwork
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+CREATE TABLE cpa
+(
+	cpa_id						VARCHAR(256)		NOT NULL UNIQUE,
+	cpa								CLOB						NOT NULL
+);
+
+CREATE TABLE url
+(
+	old_url 					VARCHAR(256)		NOT NULL UNIQUE,
+	new_url						VARCHAR(256)		NOT NULL
+);
+
+CREATE TABLE ebms_message
+(
+	time_stamp				TIMESTAMP				NOT NULL,
+	cpa_id						VARCHAR(256)		NOT NULL,
+	conversation_id		VARCHAR(256)		NOT NULL,
+	sequence_nr				SMALLINT				NULL,
+	message_id				VARCHAR(256)		NOT NULL,
+	message_nr				SMALLINT				DEFAULT 0 NOT NULL,
+	ref_to_message_id	VARCHAR(256)		NULL,
+	time_to_live			TIMESTAMP				NULL,
+	from_role					VARCHAR(256)		NULL,
+	to_role						VARCHAR(256)		NULL,
+	service						VARCHAR(256)		NOT NULL,
+	action						VARCHAR(256)		NOT NULL,
+	content						CLOB						NULL,
+	status						SMALLINT				NULL,
+	status_time				TIMESTAMP				NULL,
+	PRIMARY KEY (message_id,message_nr)
+);
+
+CREATE TABLE ebms_attachment
+(
+	message_id				VARCHAR(256)		NOT NULL,
+	message_nr				SMALLINT				NOT NULL,
+	order_nr					SMALLINT				NOT NULL,
+	name							VARCHAR(256)		NULL,
+	content_id 				VARCHAR(256) 		NOT NULL,
+	content_type			VARCHAR(255)		NOT NULL,
+	content						BLOB						NOT NULL,
+	FOREIGN KEY (message_id,message_nr) REFERENCES ebms_message(message_id,message_nr)
+);
+
+CREATE TABLE ebms_event
+(
+	message_id				VARCHAR(256)		NOT NULL,
+	time							TIMESTAMP				NOT NULL,
+	type							SMALLINT				NOT NULL,
+	status						SMALLINT				NOT NULL,
+	status_time				TIMESTAMP				NOT NULL,
+	uri								VARCHAR(256)		NULL,
+	error_message			CLOB						NULL
+);
