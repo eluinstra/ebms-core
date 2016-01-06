f5ad46a6a146daa6441dc3e78672addda35c78d3
eluinstra <e.luinstra@gmail.com>
2016-01-06 21:08:42 +0100

    cleanup

--- resources/scripts/database/2.2.1/mysql.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX i_ebms_message ON ebms_message (cpa_id(255),status,message_nr);
-
-CREATE INDEX i_ebms_event ON ebms_event (status);
--- resources/scripts/database/2.2.1/postgres.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX i_ebms_message ON ebms_message (cpa_id,status,message_nr);
-
-CREATE INDEX i_ebms_event ON ebms_event (status);
