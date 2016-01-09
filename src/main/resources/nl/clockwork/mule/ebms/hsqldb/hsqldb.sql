57c1cba55269ecc03479115591c39f62657339bf
eluinstra <e.luinstra@gmail.com>
2016-01-09 23:04:29 +0100

    release 2.10.0 upgraded ebms-core to version 2.10.0

--- src/main/resources/nl/clockwork/mule/ebms/hsqldb/hsqldb.sql
+++ src/main/resources/nl/clockwork/mule/ebms/hsqldb/hsqldb.sql
@@ -16,7 +16,7 @@
 
 CREATE TABLE cpa
 (
-	cpa_id						VARCHAR(256)		NOT NULL UNIQUE,
+	cpa_id						VARCHAR(256)		NOT NULL PRIMARY KEY,
 	cpa								CLOB						NOT NULL,
 	url								VARCHAR(256)		NULL
 );
@@ -26,19 +26,21 @@ CREATE TABLE ebms_message
 	time_stamp				TIMESTAMP				NOT NULL,
 	cpa_id						VARCHAR(256)		NOT NULL,
 	conversation_id		VARCHAR(256)		NOT NULL,
-	sequence_nr				SMALLINT				NULL,
 	message_id				VARCHAR(256)		NOT NULL,
 	message_nr				SMALLINT				DEFAULT 0 NOT NULL,
 	ref_to_message_id	VARCHAR(256)		NULL,
 	time_to_live			TIMESTAMP				NULL,
+	from_party_id			VARCHAR(256)		NOT NULL,
 	from_role					VARCHAR(256)		NULL,
+	to_party_id				VARCHAR(256)		NOT NULL,
 	to_role						VARCHAR(256)		NULL,
 	service						VARCHAR(256)		NOT NULL,
 	action						VARCHAR(256)		NOT NULL,
 	content						CLOB						NULL,
 	status						SMALLINT				NULL,
 	status_time				TIMESTAMP				NULL,
-	PRIMARY KEY (message_id,message_nr)
+	PRIMARY KEY (message_id,message_nr),
+	FOREIGN KEY (cpa_id) REFERENCES cpa(cpa_id)
 );
 
 CREATE TABLE ebms_attachment
@@ -55,11 +57,22 @@ CREATE TABLE ebms_attachment
 
 CREATE TABLE ebms_event
 (
+	cpa_id						VARCHAR(256)		NOT NULL,
+	channel_id				VARCHAR(256)		NOT NULL,
+	message_id				VARCHAR(256)		NOT NULL UNIQUE,
+	message_nr				SMALLINT				DEFAULT 0 NOT NULL,
+	time_to_live			TIMESTAMP				NULL,
+	time_stamp				TIMESTAMP				NOT NULL,
+	retries						SMALLINT				DEFAULT 0 NOT NULL,
+	FOREIGN KEY (cpa_id) REFERENCES cpa(cpa_id),
+	FOREIGN KEY (message_id,message_nr) REFERENCES ebms_message(message_id,message_nr)
+);
+
+CREATE TABLE ebms_event_log
+(
 	message_id				VARCHAR(256)		NOT NULL,
-	time							TIMESTAMP				NOT NULL,
-	type							SMALLINT				NOT NULL,
-	status						SMALLINT				NOT NULL,
-	status_time				TIMESTAMP				NOT NULL,
+	time_stamp				TIMESTAMP				NOT NULL,
 	uri								VARCHAR(256)		NULL,
+	status						SMALLINT				NOT NULL,
 	error_message			CLOB						NULL
 );
