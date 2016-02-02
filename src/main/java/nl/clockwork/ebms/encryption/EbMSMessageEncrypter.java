d22946ea675f35636fa70f21c66046b6f003b432
eluinstra <e.luinstra@gmail.com>
2016-02-02 11:05:18 +0100

    implementing encryption

--- src/main/java/nl/clockwork/ebms/encryption/EbMSMessageEncrypter.java
+++ src/main/java/nl/clockwork/ebms/encryption/EbMSMessageEncrypter.java
@@ -1,7 +1,10 @@
 package nl.clockwork.ebms.encryption;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.PublicKey;
@@ -10,6 +13,11 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import nl.clockwork.ebms.common.util.SecurityUtils;
 
@@ -18,6 +26,7 @@ import org.apache.xml.security.encryption.EncryptedKey;
 import org.apache.xml.security.encryption.XMLCipher;
 import org.apache.xml.security.keys.KeyInfo;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 public class EbMSMessageEncrypter
@@ -27,6 +36,22 @@ public class EbMSMessageEncrypter
 		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
 		keyGenerator.init(128);
 		return keyGenerator.generateKey();
+	}
+
+	private static void outputDocToFile(Document doc, String fileName) throws Exception
+	{
+		File encryptionFile = new File(fileName);
+		FileOutputStream f = new FileOutputStream(encryptionFile);
+
+		TransformerFactory factory = TransformerFactory.newInstance();
+		Transformer transformer = factory.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION,"yes");
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(f);
+		transformer.transform(source,result);
+
+		f.close();
+		System.out.println("Wrote document containing decrypted data to " + encryptionFile.toURI().toURL().toString());
 	}
 
 	public static void main(String[] args) throws Exception
@@ -51,14 +76,28 @@ public class EbMSMessageEncrypter
 		XMLCipher xmlCipher = XMLCipher.getInstance(XMLCipher.AES_256);
 		xmlCipher.init(XMLCipher.ENCRYPT_MODE,secretKey);
 
-    EncryptedData encryptedData = xmlCipher.getEncryptedData();
-    KeyInfo keyInfo = new KeyInfo(document);
-    keyInfo.add(encryptedKey);
-    encryptedKey.setKeyInfo(new KeyInfo(builder.parse(new InputSource(new StringReader("<KeyName>CN=52487C45.cm-4-1b.dynamic.ziggo.nl, serialNumber=00000001820029336000, O=Ordina, C=NL</KeyName>")))));
-    encryptedData.setKeyInfo(keyInfo);
+		EncryptedData encryptedData = xmlCipher.getEncryptedData();
+		KeyInfo keyInfo = new KeyInfo(document);
+		keyInfo.add(encryptedKey);
+		encryptedKey.setKeyInfo(new KeyInfo(builder.parse(new InputSource(new StringReader("<KeyName>CN=52487C45.cm-4-1b.dynamic.ziggo.nl, serialNumber=00000001820029336000, O=Ordina, C=NL</KeyName>")))));
+		encryptedData.setKeyInfo(keyInfo);
 
-    EncryptedData encryptData = xmlCipher.encryptData(null,null,new FileInputStream("/home/edwin/Downloads/A1453383414677.12095612@ebms.cv.prod.osb.overheid.nl_cn.decrypted.xml"));
-    
-//		IOUtils.write(XMLMessageBuilder.getInstance(EncryptedData.class).handle(encryptData),new FileOutputStream("/home/edwin/Downloads/A1453383414677.12095612@ebms.cv.prod.osb.overheid.nl_cn.encrypted.xml"));
+		//xmlCipher.doFinal(document,document.getDocumentElement(),true);
+
+		//outputDocToFile(document,"/home/edwin/Downloads/A1453383414677.12095612@ebms.cv.prod.osb.overheid.nl_cn.encrypted.2.xml");
+
+		encryptedData = xmlCipher.encryptData(null,null,new FileInputStream("/home/edwin/Downloads/A1453383414677.12095612@ebms.cv.prod.osb.overheid.nl_cn.decrypted.xml"));
+		Element element = xmlCipher.martial(document,encryptedData);
+		//System.out.println(DOMUtils.toString((Document)element));
+
+		TransformerFactory transFactory = TransformerFactory.newInstance();
+		Transformer transformer = transFactory.newTransformer();
+		StringWriter buffer = new StringWriter();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		transformer.transform(new DOMSource(element),new StreamResult(buffer));
+		System.out.println(buffer.toString());
+		
+
+		// IOUtils.write(XMLMessageBuilder.getInstance(EncryptedData.class).handle(encryptData),new FileOutputStream("/home/edwin/Downloads/A1453383414677.12095612@ebms.cv.prod.osb.overheid.nl_cn.encrypted.xml"));
 	}
 }
