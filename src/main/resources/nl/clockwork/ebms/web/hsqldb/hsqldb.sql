b067d6fce72b63b3c89e8a2dec18e69da0d776a0
eluinstra <e.luinstra@gmail.com>
2016-02-02 10:19:23 +0100

    upgraded ebms-core to version 2.10.5

--- src/main/resources/nl/clockwork/ebms/web/hsqldb/hsqldb.sql
+++ src/main/resources/nl/clockwork/ebms/web/hsqldb/hsqldb.sql
@@ -16,14 +16,9 @@
 
 CREATE TABLE cpa
 (
-	cpa_id						VARCHAR(256)		NOT NULL UNIQUE,
-	cpa								CLOB						NOT NULL
-);
-
-CREATE TABLE url
-(
-	old_url 					VARCHAR(256)		NOT NULL UNIQUE,
-	new_url						VARCHAR(256)		NOT NULL
+	cpa_id						VARCHAR(256)		NOT NULL PRIMARY KEY,
+	cpa								CLOB						NOT NULL,
+	url								VARCHAR(256)		NULL
 );
 
 CREATE TABLE ebms_message
@@ -31,25 +26,28 @@ CREATE TABLE ebms_message
 	time_stamp				TIMESTAMP				NOT NULL,
 	cpa_id						VARCHAR(256)		NOT NULL,
 	conversation_id		VARCHAR(256)		NOT NULL,
-	sequence_nr				SMALLINT				NULL,
 	message_id				VARCHAR(256)		NOT NULL,
 	message_nr				SMALLINT				DEFAULT 0 NOT NULL,
 	ref_to_message_id	VARCHAR(256)		NULL,
 	time_to_live			TIMESTAMP				NULL,
+	from_party_id			VARCHAR(256)		NOT NULL,
 	from_role					VARCHAR(256)		NULL,
+	to_party_id				VARCHAR(256)		NOT NULL,
 	to_role						VARCHAR(256)		NULL,
 	service						VARCHAR(256)		NOT NULL,
 	action						VARCHAR(256)		NOT NULL,
 	content						CLOB						NULL,
 	status						SMALLINT				NULL,
 	status_time				TIMESTAMP				NULL,
-	PRIMARY KEY (message_id,message_nr)
+	PRIMARY KEY (message_id,message_nr),
+	FOREIGN KEY (cpa_id) REFERENCES cpa(cpa_id)
 );
 
 CREATE TABLE ebms_attachment
 (
 	message_id				VARCHAR(256)		NOT NULL,
 	message_nr				SMALLINT				NOT NULL,
+	order_nr					SMALLINT				NOT NULL,
 	name							VARCHAR(256)		NULL,
 	content_id 				VARCHAR(256) 		NOT NULL,
 	content_type			VARCHAR(255)		NOT NULL,
@@ -59,11 +57,22 @@ CREATE TABLE ebms_attachment
 
 CREATE TABLE ebms_event
 (
+	cpa_id						VARCHAR(256)		NOT NULL,
+	channel_id				VARCHAR(256)		NOT NULL,
+	message_id				VARCHAR(256)		NOT NULL UNIQUE,
+	message_nr				SMALLINT				DEFAULT 0 NOT NULL,
+	time_to_live			TIMESTAMP				NULL,
+	time_stamp				TIMESTAMP				NOT NULL,
+	retries						SMALLINT				DEFAULT 0 NOT NULL,
+	FOREIGN KEY (cpa_id) REFERENCES cpa(cpa_id),
+	FOREIGN KEY (message_id,message_nr) REFERENCES ebms_message(message_id,message_nr)
+);
+
+CREATE TABLE ebms_event_log
+(
 	message_id				VARCHAR(256)		NOT NULL,
-	time							TIMESTAMP				DEFAULT NOW() NOT NULL,
-	type							SMALLINT				NOT NULL,
-	status						SMALLINT				DEFAULT 0 NOT NULL,
-	status_time				TIMESTAMP				DEFAULT NOW() NOT NULL,
+	time_stamp				TIMESTAMP				NOT NULL,
 	uri								VARCHAR(256)		NULL,
+	status						SMALLINT				NOT NULL,
 	error_message			CLOB						NULL
 );
