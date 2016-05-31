2df2116760fd5d70244c33efd0079b29e624cba4
eluinstra <e.luinstra@gmail.com>
2016-06-01 00:39:13 +0200

    added getLastReceivedMessage

--- src/main/java/nl/clockwork/ebms/dao/EbMSDAO.java
+++ src/main/java/nl/clockwork/ebms/dao/EbMSDAO.java
@@ -53,6 +53,7 @@ public interface EbMSDAO
 	EbMSMessageContent getMessageContent(String messageId) throws DAOException;
 	EbMSMessageContext getMessageContext(String messageId) throws DAOException;
 	EbMSMessageContext getMessageContextByRefToMessageId(String cpaId, String refToMessageId, Service service, String...actions) throws DAOException;
+	EbMSMessageContext getLastReceivedMessage(String cpaId, String conversationId);
 	Document getDocument(String messageId) throws DAOException;
 	EbMSDocument getEbMSDocumentIfUnsent(String messageId) throws DAOException;
 	EbMSDocument getEbMSDocumentByRefToMessageId(String cpaId, String refToMessageId, Service service, String...actions) throws DAOException;
