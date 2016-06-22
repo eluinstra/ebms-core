c692993a50cca7427478b38b8f64bda5c7fc057d
eluinstra <e.luinstra@gmail.com>
2016-06-22 21:19:29 +0200

    removed certificate validity check

--- src/main/java/nl/clockwork/ebms/validation/ClientCertificateValidator.java
+++ src/main/java/nl/clockwork/ebms/validation/ClientCertificateValidator.java
@@ -1,14 +1,9 @@
 package nl.clockwork.ebms.validation;
 
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.Date;
 
 import nl.clockwork.ebms.common.CPAManager;
-import nl.clockwork.ebms.common.KeyStoreManager;
-import nl.clockwork.ebms.common.util.SecurityUtils;
 import nl.clockwork.ebms.model.CacheablePartyId;
 import nl.clockwork.ebms.model.EbMSMessage;
 import nl.clockwork.ebms.util.CPAUtils;
@@ -17,41 +12,25 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oasis_open.committees.ebxml_cppa.schema.cpp_cpa_2_0.DeliveryChannel;
 import org.oasis_open.committees.ebxml_msg.schema.msg_header_2_0.MessageHeader;
-import org.springframework.beans.factory.InitializingBean;
 
-public class ClientCertificateValidator implements InitializingBean
+public class ClientCertificateValidator
 {
 	protected transient Log logger = LogFactory.getLog(getClass());
 	private boolean enabled;
 	private CPAManager cpaManager;
-	private String trustStorePath;
-	private String trustStorePassword;
-	private KeyStore trustStore;
-
-	@Override
-	public void afterPropertiesSet() throws Exception
-	{
-		trustStore = KeyStoreManager.getKeyStore(trustStorePath,trustStorePassword);
-	}
 
 	public void validate(EbMSMessage message) throws ValidatorException
 	{
 		if (enabled)
-			try
+		{
+			X509Certificate[] certificates = ClientCertificateManager.getCertificates();
+			if (certificates != null && certificates.length > 0)
 			{
-				X509Certificate[] certificates = ClientCertificateManager.getCertificates();
-				if (certificates != null && certificates.length > 0)
-				{
-					java.security.cert.X509Certificate certificate = certificates[0];
-					if (!certificate.equals(getClientCertificate(message.getMessageHeader())))
-						throw new ValidationException("Invalid SSL Client Certificate!");
-					SecurityUtils.validateCertificate(trustStore,certificate,message.getMessageHeader().getMessageData().getTimestamp() == null ? new Date() : message.getMessageHeader().getMessageData().getTimestamp());
-				}
+				java.security.cert.X509Certificate certificate = certificates[0];
+				if (!certificate.equals(getClientCertificate(message.getMessageHeader())))
+					throw new ValidationException("Invalid SSL Client Certificate!");
 			}
-			catch (GeneralSecurityException e)
-			{
-				new ValidatorException(e);
-			}
+		}
 	}
 
 	private java.security.cert.X509Certificate getClientCertificate(MessageHeader messageHeader)
@@ -78,16 +57,6 @@ public class ClientCertificateValidator implements InitializingBean
 	public void setCpaManager(CPAManager cpaManager)
 	{
 		this.cpaManager = cpaManager;
-	}
-
-	public void setTrustStorePath(String trustStorePath)
-	{
-		this.trustStorePath = trustStorePath;
-	}
-
-	public void setTrustStorePassword(String trustStorePassword)
-	{
-		this.trustStorePassword = trustStorePassword;
 	}
 
 }
