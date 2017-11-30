33484db1d93445b46757074d2dfd0d65e58c3bd8
eluinstra <e.luinstra@gmail.com>
2017-11-30 16:36:57 +0100

    added missing tables

--- resources/scripts/database/master/hsqldb.drop.sql
+++ resources/scripts/database/master/hsqldb.drop.sql
@@ -1,3 +1,5 @@
+DROP TABLE ebms_message_event;
+
 DROP TABLE ebms_event_log;
 
 DROP TABLE ebms_event;
@@ -7,3 +9,5 @@ DROP TABLE ebms_attachment;
 DROP TABLE ebms_message;
 
 DROP TABLE cpa;
+
+DROP TABLE url;
