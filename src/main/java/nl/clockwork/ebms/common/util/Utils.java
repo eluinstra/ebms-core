99019e6b35ac95897deba92c22a3b03df0953899
eluinstra <e.luinstra@gmail.com>
2019-08-13 18:18:43 +0200

    fixed NumberFormatException on spring properties
    Conflicts:
    	src/main/java/nl/clockwork/ebms/client/EbMSHttpClientFactory.java

---
+++ src/main/java/nl/clockwork/ebms/common/util/Utils.java
@@ -0,0 +1,21 @@
+package nl.clockwork.ebms.common.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Utils
+{
+	public static List<Integer> getIntegerList(String input)
+	{
+		List<Integer> result = new ArrayList<>();
+		if (!StringUtils.isEmpty(input))
+		{
+			String[] strings = input.split(",");
+			for (String s : strings)
+				result.add(Integer.parseInt(s.trim()));
+		}
+		return result;
+	}
+}
