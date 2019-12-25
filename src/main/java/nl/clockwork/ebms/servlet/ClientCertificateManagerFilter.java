cd2064c005da21ce0db79da5ed3b9ee11e4a0846
eluinstra <e.luinstra@gmail.com>
2019-12-25 22:22:37 +0100

    fixed filter

--- src/main/java/nl/clockwork/ebms/servlet/ClientCertificateManagerFilter.java
+++ src/main/java/nl/clockwork/ebms/servlet/ClientCertificateManagerFilter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Clockwork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.clockwork.ebms.servlet;
 
 import java.io.ByteArrayInputStream;
@@ -46,6 +61,7 @@ public class ClientCertificateManagerFilter implements Filter
 				X509Certificate certificate = decode(request.getAttribute(x509CertificateHeader));
 				ClientCertificateManager.setCertificate(certificate);
 			}
+			chain.doFilter(request,response);
 		}
 		catch (CertificateException e)
 		{
