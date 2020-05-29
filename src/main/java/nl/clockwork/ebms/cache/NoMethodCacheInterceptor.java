340e99464b41f52e59409d1458b2d7f2691a0ccd
eluinstra <e.luinstra@gmail.com>
2020-05-29 18:34:34 +0200

    implemented NoMethodCacheInterceptor

---
+++ src/main/java/nl/clockwork/ebms/cache/NoMethodCacheInterceptor.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2011 Clockwork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.clockwork.ebms.cache;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@NoArgsConstructor
+public class NoMethodCacheInterceptor implements MethodInterceptor
+{
+	public Object invoke(MethodInvocation invocation) throws Throwable
+	{
+		return invocation.proceed();
+	}
+}
