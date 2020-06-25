a4376419cc33313c7a150c014cf898be89e99017
eluinstra <e.luinstra@gmail.com>
2020-06-25 21:31:50 +0200

    fixed concurrency issue

--- src/main/java/nl/clockwork/ebms/event/processor/EbMSSendEventListener.java
+++ src/main/java/nl/clockwork/ebms/event/processor/EbMSSendEventListener.java
@@ -42,13 +42,21 @@ public class EbMSSendEventListener implements MessageListener
 		try
 		{
 			val event = createEvent(message);
-			val task = handleEventTaskBuilder.event(event).build();
+			val task = createTask(event);
 			task.run();
 		}
 		catch (JMSException e)
 		{
 			log.error("",e);
 			throw new RuntimeException(e);
+		}
+	}
+
+	private HandleEventTask createTask(final nl.clockwork.ebms.event.processor.EbMSEvent event)
+	{
+		synchronized (this)
+		{
+			return handleEventTaskBuilder.event(event).build();
 		}
 	}
 
