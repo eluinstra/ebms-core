3ce6b4c64059eb5313fdb7dcd4b3ace596ea218c
John Wentink <john.wentink@gmail.com>
2021-06-14 12:38:28 +0200

    implemented a fix proposed by Lost Lemon, a too strict xsd validation causes the adapter to break at startup. added XXE vuln test taken from the 2.17 branch

--- src/main/java/nl/clockwork/ebms/validation/XSDValidator.java
+++ src/main/java/nl/clockwork/ebms/validation/XSDValidator.java
@@ -44,8 +44,8 @@ public class XSDValidator
 		{
 			val factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD,"");
-			factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA,"");
-      val systemId = this.getClass().getResource(xsdFile).toString();
+			factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA,"file");
+			val systemId = this.getClass().getResource(xsdFile).toString();
 			schema = factory.newSchema(new StreamSource(this.getClass().getResourceAsStream(xsdFile),systemId));
 		}
 		catch (SAXException e)
