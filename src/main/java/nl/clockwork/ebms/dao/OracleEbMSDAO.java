294441b3199b95355b5a76a43fab6367eff9f974
eluinstra <eluinstra@luin.dev>
2022-11-12 18:43:53 +0100

    fixed Oracle BigInteger issue

--- src/main/java/nl/clockwork/ebms/dao/OracleEbMSDAO.java
+++ src/main/java/nl/clockwork/ebms/dao/OracleEbMSDAO.java
@@ -15,10 +15,29 @@
  */
 package nl.clockwork.ebms.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.support.TransactionTemplate;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
 
+import javax.xml.transform.TransformerException;
+
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.w3c.dom.Document;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import lombok.val;
 import nl.clockwork.ebms.EbMSMessageStatus;
+import nl.clockwork.ebms.EbMSMessageUtils;
+import nl.clockwork.ebms.model.EbMSAttachment;
+import nl.clockwork.ebms.model.EbMSBaseMessage;
+import nl.clockwork.ebms.util.DOMUtils;
 
 class OracleEbMSDAO extends AbstractEbMSDAO
 {
@@ -39,4 +58,67 @@ class OracleEbMSDAO extends AbstractEbMSDAO
 		" order by time_stamp asc)" +
 		" where ROWNUM <= " + maxNr;
 	}
+
+	@Override
+	public Tuple2<String,Integer> insertDuplicateMessage(final Instant timestamp, final Document document, final EbMSBaseMessage message, final List<EbMSAttachment> attachments)
+	{
+		try
+		{
+			val keyHolder = new GeneratedKeyHolder();
+			jdbcTemplate.update(c ->
+			{
+				try
+				{
+					val messageHeader = message.getMessageHeader();
+					val ps = c.prepareStatement
+					(
+						"insert into ebms_message (" +
+							"time_stamp," +
+							"cpa_id," +
+							"conversation_id," +
+							"message_id," +
+							"message_nr," +
+							"ref_to_message_id," +
+							"time_to_live," +
+							"from_party_id," +
+							"from_role," +
+							"to_party_id," +
+							"to_role," +
+							"service," +
+							"action," +
+							"content" +
+						") values (?,?,?,?,(select max(message_nr) + 1 from ebms_message where message_id = ?),?,?,?,?,?,?,?,?,?)",
+						new int[]{4,5}
+					);
+					ps.setTimestamp(1,Timestamp.from(timestamp));
+					ps.setString(2,messageHeader.getCPAId());
+					ps.setString(3,messageHeader.getConversationId());
+					ps.setString(4,messageHeader.getMessageData().getMessageId());
+					ps.setString(5,messageHeader.getMessageData().getMessageId());
+					ps.setString(6,messageHeader.getMessageData().getRefToMessageId());
+					ps.setTimestamp(7,messageHeader.getMessageData().getTimeToLive() == null ? null : Timestamp.from(messageHeader.getMessageData().getTimeToLive()));
+					ps.setString(8,EbMSMessageUtils.toString(messageHeader.getFrom().getPartyId().get(0)));
+					ps.setString(9,messageHeader.getFrom().getRole());
+					ps.setString(10,EbMSMessageUtils.toString(messageHeader.getTo().getPartyId().get(0)));
+					ps.setString(11,messageHeader.getTo().getRole());
+					ps.setString(12,EbMSMessageUtils.toString(messageHeader.getService()));
+					ps.setString(13,messageHeader.getAction());
+					ps.setString(14,DOMUtils.toString(document,"UTF-8"));
+					return ps;
+				}
+				catch (TransformerException e)
+				{
+					throw new SQLException(e);
+				}
+			},
+			keyHolder);
+			insertAttachments(keyHolder,attachments);
+			return Tuple.of((String)keyHolder.getKeys().get("message_id"),((BigDecimal)keyHolder.getKeys().get("message_nr")).intValue());
+		}
+		catch (IOException e)
+		{
+			throw new DataRetrievalFailureException("",e);
+		}
+	}
+
 }
