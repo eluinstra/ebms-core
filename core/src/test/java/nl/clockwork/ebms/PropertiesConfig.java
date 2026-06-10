52c7a9a2f9bbbc5ec6676216e48ff481544c4304
eluinstra <edwin.luinstra@soprasteria.nl>
2026-06-10 21:14:42 +0000

    fixed tests

--- core/src/test/java/nl/clockwork/ebms/PropertiesConfig.java
+++ core/src/test/java/nl/clockwork/ebms/PropertiesConfig.java
@@ -15,11 +15,18 @@
  */
 package nl.clockwork.ebms;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @PropertySource(value = {"classpath:nl/clockwork/ebms/default.properties"}, ignoreResourceNotFound = false)
 public class PropertiesConfig
 {
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer()
+	{
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 }
