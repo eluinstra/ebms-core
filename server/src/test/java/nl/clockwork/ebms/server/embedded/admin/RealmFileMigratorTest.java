5a36e7c7d12970ba99983dde653fa4cd706fbe93
eluinstra <edwin.luinstra@soprasteria.nl>
2026-06-16 15:24:29 +0000

    improvements

--- server/src/test/java/nl/clockwork/ebms/server/embedded/admin/RealmFileMigratorTest.java
+++ server/src/test/java/nl/clockwork/ebms/server/embedded/admin/RealmFileMigratorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.clockwork.ebms.server.embedded;
+package nl.clockwork.ebms.server.embedded.admin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
